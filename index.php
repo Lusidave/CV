3294d2f3fcfb2e6ba58a2c4a56c9f47de139f11b
Lusidave <lusidave@hotmail.fr>
2020-03-28 15:41:05 +0100

    deplacement fichier

--- index.php
+++ index.php
@@ -33,63 +33,9 @@ include('./src/header.php');
     </div>
 </section>
 
-<section id="skills">
-    <div class="red-divider"></div>
-    <div class="heading">
-        <h2>Compétences</h2>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
-                        <h5>HTML 90%</h5>
-                    </div>
-                </div>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
-                        <h5>CSS 90%</h5>
-                    </div>
-                </div>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
-                        <h5>JAVASCRIPT 40%</h5>
-                    </div>
-                </div>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:65%">
-                        <h5>GITHUB 65%</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:75%">
-                        <h5>PHP 75%</h5>
-                    </div>
-                </div>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width:55%">
-                        <h5>MYSQL 55%</h5>
-                    </div>
-                </div>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
-                        <h5>BOOTSTRAP 70%</h5>
-                    </div>
-                </div>
-
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
-                        <h5>WILD CODE SCHOOL 40%</h5>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-    </div>
-</section>
+<?php
+include"competence.php";
+?>
 
 <section id="experience">
 
