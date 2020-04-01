9f444daf7d9cf1a457d8f2f066c5d0b07cef9249
Lusidave <lusidave@hotmail.fr>
2020-04-01 23:38:56 +0200

    base de donnees mysql

--- src/form.php
+++ src/form.php
@@ -1,5 +1,5 @@
 <?php
-    $firstname = $name = $email = $phone = $message = "";
+  /*  $firstname = $name = $email = $phone = $message = "";
     $firstnameError = $nameError = $emailError = $phoneError = $messageError = "";
     $isSuccess = false;
     $emailTo = "lusidave@gmail.com";
@@ -105,10 +105,15 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
 }
 
 
+*/
+require_once 'connec.php';
 
+$query = "SELECT * FROM userForm";
+$statement = $pdo->query($query);
+$cvLuzi = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<section id="contact">
 
+<section id="contact">
 <div class="container">
     <div class="red-divider"></div>
     <div class="heading">
@@ -116,38 +121,40 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
     </div>
     <div class="row">
         <div class="col-lg-10 col-lg-offset-1">
-            <form enctype="multipart/form-data" id="contact-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" role="form">
+            <form enctype="multipart/form-data" id="contact-form" method="post" action="#contact" role="form">
+                 <?php /*echo htmlspecialchars($_SERVER['PHP_SELF']);*/ ?>
                 <div class="row">
 
 
                     <div class="col-md-6">
                         <label for="firstname">Prénom<span class="blue"> *</span></label>
-                        <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom" value="<?php echo $firstname; ?>">
-                        <p class="comments"><?php echo $firstnameError; ?></p>
+                        <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom" value=""> <?php /*echo $firstname;*/ ?>
+                        <p class="comments"></p>
+                        <?php /*echo $firstnameError;*/ ?>
                     </div>
 
                     <div class="col-md-6">
                         <label for="name">Nom<span class="blue"> *</span></label>
-                        <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom" value="<?php echo $name; ?>">
-                        <p class="comments"><?php echo $nameError; ?></p>
+                        <input type="text" id="name" name="nom" class="form-control" placeholder="Votre nom" value="">
+                        <p class="comments"></p>
                     </div>
 
                     <div class="col-md-6">
                         <label for="email">Email<span class="blue"> *</span></label>
-                        <input type="email" id="email" name="email" class="form-control" placeholder="Votre email" value="<?php echo $email; ?>">
-                        <p class="comments"><?php echo $emailError; ?></p>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Votre email" value="">
+                        <p class="comments"></p>
                     </div>
 
                     <div class="col-md-6">
                         <label for="phone">Téléphone</label>
-                        <input type="tel" id="phone" name="phone" class="form-control" placeholder="Votre téléphone" value="<?php echo $phone; ?>">
-                        <p class="comments"><?php echo $phoneError; ?></p>
+                        <input type="tel" id="phone" name="phone" class="form-control" placeholder="Votre téléphone" value="">
+                        <p class="comments"></p>
                     </div>
 
                     <div class="col-md-12">
                         <label for="message">Message<span class="blue"> *</span></label>
-                        <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4"><?php echo $message; ?></textarea>
-                        <p class="comments"><?php echo $messageError; ?></p>
+                        <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4"></textarea>
+                        <p class="comments"></p>
                     </div>
 
                     <div id="sendfile" class="col-md-12">
@@ -160,15 +167,37 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
                     </div>
 
                     <div class="col-md-12">
-                       <input type="submit" class="button1" value="Envoyer">
+                       <input type="submit" id="send" name="send" class="button1" value="Envoyer">
                     </div>
-
-
                 </div>
-                <p class="thank-you" style="display:<?php if($isSuccess) echo 'block'; else echo 'none';?>">Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>
+                <p class="thank-you" >Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>
+               <!-- style="display:<?php /*if($isSuccess) echo 'block'; else echo 'none';*/?>" -->
             </form>
         </div>
     </div>
 
 </div>
 </section>
+
+<?php
+
+if($_POST['send']=='send'){
+
+    $firstname = trim($_POST['firstname']);
+    $nom = trim($_POST['nom']);
+    $email = trim($_POST['email']);
+    $phone = trim($_POST['phone']);
+    $message = trim($_POST['message']);
+    $query = 'INSERT INTO userForm (firstname, nom, email, phone, message) VALUES (:firstname, :nom, :email, :phone, :message)';
+    $statement = $pdo->prepare($query);
+
+    $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+    $statement->bindValue(':nom', $nom, \PDO::PARAM_STR);
+    $statement->bindValue(':email', $email, \PDO::PARAM_STR);
+    $statement->bindValue(':phone', $phone, \PDO::PARAM_INT);
+    $statement->bindValue(':message', $message, \PDO::PARAM_STR);
+
+
+    $statement->execute();
+};
+?>
